0d775e1938244912e768824dc17cacc8b882b670
Decargarbondoee <edecargarbondoe@gmail.com>
2022-10-13 18:15:22 -0700

    C code and save it in an output file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
-\**
-*main = Entry
-*Return: Always 0
 
-*/ 
+
+\**
+
+
+* main = printing
+
+* Return: Always 0 (successful)
+*
+7
+
 int main(void)
 {
-	puts("\"progrsmming is like a building a multilingual puzzed") 
-		return (0);
+	puts(""\"programming is like a building a multilingual puzzed"); 
+	
+	return (0);
+}
