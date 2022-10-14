1f045ecd4c477f629637cbc57a7c13d6a10f6754
Decargarbondoee <edecargarbondoe@gmail.com>
2022-10-13 17:14:51 -0700

    alx-low_level_programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+
+*main - printing  
+
+*
+*Return Aways 0 (Successful)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
