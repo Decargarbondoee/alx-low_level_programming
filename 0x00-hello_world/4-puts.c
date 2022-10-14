e9c14e5aa3169e6450f966a1a5ced1dfb93c35ef
Decargarbondoee <edecargarbondoe@gmail.com>
2022-10-13 17:40:55 -0700

    my first 0x00-hello_world commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
+\**
+*main = Entry
+*Return: Always 0
 
-/**
-
-*main - printing  
-
-*
-*Return Aways 0 (Successful)
-*/
-
+*/ 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-
-}
+	puts("\"progrsmming is like a building a multilingual puzzed") 
+		return (0);
