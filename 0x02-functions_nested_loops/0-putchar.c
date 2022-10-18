abe493c7bd2b623879756e80ed63ee8656bf489c
Decargarbondoee <edecargarbondoe@gmail.com>
2022-10-18 07:50:38 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	char a = "Emmett";
+
+	while (a)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
