3ceac928babf854f90fec54921935c59f18b4661
Decargarbondoee <edecargarbondoe@gmail.com>
2022-10-18 18:40:12 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ * @n: the number
+ * Return: value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int id = n % 10;
+
+	if (id < 0)
+		id *= -1;
+	_putchar(id * '0');
+
+	return (0);
+}
