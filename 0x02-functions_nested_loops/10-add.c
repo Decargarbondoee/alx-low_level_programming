da2c0bc2583ffbfb71352d48d8dce3e278db13d0
Decargarbondoee <edecargarbondoe@gmail.com>
2022-10-19 17:23:44 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - int two remainder
+ * @a: the first remainder
+ * @b: the second remainder
+ * Return: the additional of both
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
