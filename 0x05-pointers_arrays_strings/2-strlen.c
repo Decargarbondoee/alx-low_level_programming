2723f789d28d8e18da23ab42253bf6dc3e722a4d
Decargarbondoee <edecargarbondoe@gmail.com>
2022-10-24 09:47:42 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen -  length of a string
+ * @s: string
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int le = 0;
+
+	while (*s != "\0")
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
