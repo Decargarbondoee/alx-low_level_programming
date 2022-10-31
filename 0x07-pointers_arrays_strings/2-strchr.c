836d0dada9aa4138044e68e9d4a283ba2bb1b494
Decargarbondoee <edecargarbondoe@gmail.com>
2022-10-31 16:46:49 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+nclude "main.h"
+#include <stddef>
+
+/**
+ * _strchr - locates a character
+ * @s: The string
+ * @c: The character
+ * Return: A pointer to the char or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	if (s == NULL)
+		return (NULL);
+
+	for (; *s; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (NULL);
+}
