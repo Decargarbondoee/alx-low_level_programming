c3e699d5502757fc1a872561ee7b65d052160bb3
Decargarbondoee <edecargarbondoe@gmail.com>
2022-10-31 17:07:12 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - Get the lenght of the priffix
+ * @s: string
+ * @accept: string compare to
+ * Return: unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+
+	for (; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+
+			if (s[i] != accept[j])
+			break;
+		}
+	}
+	return (i);
+
+}
