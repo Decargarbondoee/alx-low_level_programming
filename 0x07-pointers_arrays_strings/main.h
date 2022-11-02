ae8fefc07c4e38419e2cb9a38da70078d28e8d99
Decargarbondoee <edecargarbondoe@gmail.com>
2022-11-01 18:56:11 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@
  * Auth: E-Tech (Emmett D. Bondoe)
  * Description: Header file containing prototypes for all functions
  */
+int _putchar(char c); 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -13,6 +14,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-int _putchar(char c);
+void set_string(char **s, char *to);
 
 #endif
